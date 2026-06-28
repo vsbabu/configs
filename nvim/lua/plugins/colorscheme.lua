2eb522985a8557facd45690cef032556874b8b6f
SV <vsbabu@gmail.com>
2026-06-28 08:09:40 +0530

    Added dracula scheme to nvim; removed tmux-continuum
    Continuum never worked for me. Resurrect is good enough with manual save and restore. May be create a script to start session on reboot.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,13 @@
 return {
-  { "EdenEast/nightfox.nvim" },
-  { "rmehri01/onenord.nvim" },
-  { "ayu-theme/ayu-vim" },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      ayucolor = "mirage",
-      colorscheme = "catppuccin-frappe",
-    },
-  },
+	{ "EdenEast/nightfox.nvim" },
+	{ "rmehri01/onenord.nvim" },
+	{ "ayu-theme/ayu-vim" },
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			ayucolor = "mirage",
+			colorscheme = "catppuccin-frappe",
+		},
+	},
 }
